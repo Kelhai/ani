691cf9d0bed2d1977c6c3c1ba896384dcfca5d40
Kelhai <69177904+Kelhai@users.noreply.github.com>
2026-04-24 22:40:33 -0400

    Testing updates

--- generateCerts.sh
+++ generateCerts.sh
@@ -1,3 +1,3 @@
 openssl req -x509 -newkey ed25519 -keyout key.pem -out cert.pem -days 3650 -nodes \
-  -subj "/CN=localhost" \
-  -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"
+  -subj "/CN=134.228.142.72" \
+  -addext "subjectAltName=IP:134.228.142.72,DNS:localhost"
